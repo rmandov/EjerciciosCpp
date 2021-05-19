b2dfeee5af646d2f02e8bf0d2af1b8a8b8c8a961
Armando Velasco <armando.blye@gmail.com>
2021-05-19 12:20:45 -0500

    suma

--- swap_values.cpp
+++ swap_values.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout<<"Hello world";
+int main()
+{
+    // program for swapping values of two variables
+    
+    int a = 20;
+    int b = 10;
+    
+    a = a + b; // 30
+    b = a - b; // 20
+    a = a - b; // 10
+
+    cout << "a = " << a << endl;
+    cout << "b = " << b;
+    
     system("pause>0");
 }
